7e8dad4f3fa873872baa3c8edf7cf3f6b170cfc1
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-18 15:57:02 +0300

    update algorithm

--- Program.cs
+++ Program.cs
@@ -5,67 +5,41 @@
 		public class NumberIncreasingPathsGrid
 		{
 			private int modulo = 1000000007;
+			private int[] deltaR = new int[] { 0, -1, 0, 1 };
+			private int[] deltaC = new int[] { -1, 0, 1, 0 };
 
-			private List<int[]> GetNeighbors(int[][] grid, int r, int c)
+			private int DFS(int[,] dp, int[][] grid, int r, int c, int rows, int cols)
 			{
-				int[] deltaR = new int[] { 0, -1, 0, 1 };
-				int[] deltaC = new int[] { -1, 0, 1, 0 };
-				List<int[]> neighbors = new();
+				if (dp[r, c] != 0)
+				{
+					return dp[r, c];
+				}
+
+				int countPaths = 1;
 				for (int i = 0; i < deltaR.Length; ++i)
 				{
 					int newR = r + deltaR[i];
 					int newC = c + deltaC[i];
-					if (newR >= 0 && newR < grid.Length && newC >= 0 && newC < grid[newR].Length && grid[newR][newC] > grid[r][c])
+					if (newR >= 0 && newR < rows && newC >= 0 && newC < cols && grid[newR][newC] > grid[r][c])
 					{
-						neighbors.Add(new int[] { newR, newC });
+						countPaths += DFS(dp, grid, newR, newC, rows, cols);
+						countPaths %= modulo;
 					}
 				}
-				return neighbors;
-			}
-
-			private void BFS(int[][] dp, int[][] grid, int r, int c)
-			{
-				Queue<int[]> queue = new();
-				bool[,] visited = new bool[grid.Length, grid[0].Length];
-				queue.Enqueue(new int[] { r, c });
-				while(queue.Count > 0)
-				{
-					int[] cell = queue.Dequeue();
-					List<int[]> neighbors = GetNeighbors(grid, cell[0], cell[1]);
-					foreach (int[] neighbor in neighbors)
-					{
-						if (!visited[neighbor[0], neighbor[1]])
-						{
-							visited[neighbor[0], neighbor[1]] = true;
-							dp[neighbor[0]][neighbor[1]] = (dp[neighbor[0]][neighbor[1]] + 1) % modulo;
-							queue.Enqueue(new int[] { neighbor[0], neighbor[1] });
-						}
-					}
-				}
+				dp[r, c] = countPaths;
+				return countPaths;
 			}
 			public int CountPaths(int[][] grid)
 			{
-				int n = grid.Length;
-				int m = grid[0].Length;
-				int[][] dp = new int[n][];
-				for (int r = 0; r < n; ++r)
-				{
-					dp[r] = new int[m];
-					Array.Fill(dp[r], 1);
-				}
-				for (int r = 0; r < n; ++r)
-				{
-					for (int c = 0; c < m; ++c)
-					{
-						BFS(dp, grid, r, c);
-					}
-				}
+				int rows = grid.Length;
+				int cols = grid[0].Length;
+				int[,] dp = new int[rows, cols];
 				int countPaths = 0;
-				for (int r = 0; r < n; ++r)
+				for (int r = 0; r < rows; ++r)
 				{
-					for (int c = 0; c < m; ++c)
+					for (int c = 0; c < cols; ++c)
 					{
-						countPaths = (countPaths + dp[r][c]) % modulo;
+						countPaths += DFS(dp, grid, r, c, rows, cols);
 					}
 				}
 				return countPaths;
