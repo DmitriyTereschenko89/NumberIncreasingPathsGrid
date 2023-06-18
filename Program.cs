89ad0e7dd8c509bffbda65225bdc876f7103f8e3
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-18 14:55:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NumberIncreasingPathsGrid
+{
+	internal class Program
+	{
+		public class Solution
+		{
+			public int CountPaths(int[][] grid)
+			{
+
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
